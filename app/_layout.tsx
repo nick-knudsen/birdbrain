861f21afa7458639a99133fda9965fccd9d84280
Nick Knudsen <nick.knudsen14@gmail.com>
2025-06-30 15:12:17 -0400

    finishing tabs layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen 
+                name="(tabs)"
+                options={{ 
+                    headerShown: false 
+                }} 
+            />
+        </Stack>
+    );
 }
