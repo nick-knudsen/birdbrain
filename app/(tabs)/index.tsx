8769b75a67007925749fe717370d0c0eca07529f
Nick Knudsen <nick.knudsen14@gmail.com>
2025-07-02 12:04:57 -0400

    adding image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import { Image } from "expo-image";
+
+import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -8,7 +9,7 @@ export default function Index() {
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <Image source={PlaceholderImage} style={styles.image}/>
+                <ImageViewer imgSource={PlaceholderImage}/>
             </View>
             <Text style={styles.text}>Home Screen</Text>
             <Link href="/about" style={styles.button}>Go to About Screen</Link>
@@ -24,11 +25,6 @@ const styles = StyleSheet.create({
     },
     imageContainer: {
         flex: 1,
-    },
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
     },
     text: {
         color: '#ffffff',
