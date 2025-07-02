7c55a0e49b98c648cf450ec65e693f3cce9a061d
Nick Knudsen <nick.knudsen14@gmail.com>
2025-07-02 12:44:20 -0400

    styling buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,31 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons/FontAwesome";
 
 type Props = {
     label: string;
 };
 
-export default function Button({ label}: Props) {
+export default function Button({ label, theme }: Props) {
+    if (theme === 'primary') {
+        return (
+            <View
+                style={[
+                    styles.buttonContainer,
+                    { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+                ]}>
+                <Pressable
+                    style={[
+                        styles.button,
+                        { backgroundColor: '#ffffff'}
+                    ]}
+                    onPress={() => alert('Button Pressed!')}>
+                    <FontAwesome name="picture-o" size={18} color='#25292e' style={styles.buttonIcon} />
+                    <Text style={[styles.buttonLabel, {color: '#25292e' }]}> {label}</Text>
+                    </Pressable>
+                </View>
+        );
+    }
+
     return (
         <View style={styles.buttonContainer}>
             <Pressable style={styles.button} onPress={() => alert('Button Pressed!')}>
@@ -30,6 +51,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
+    },
+    buttonIcon: {
+        paddingRight: 8,
     },
     buttonLabel: {
         color: '#ffffff',
